9ab1ca6b5d347d28a36eb627c8502625995fa9aa
Jeff Benton <sikletz@gmail.com>
2019-04-28 00:20:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,8 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find { |
+      item = @@items.find { |i| i.name = item_name }
+      binding.pry
     elsif
       resp.write "Route not found"
       resp.status = 404
