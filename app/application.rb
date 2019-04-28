ae1100063ca50f873c5fb234a1884b429d89a525
Jeff Benton <sikletz@gmail.com>
2019-04-28 00:19:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item = req.path.split("/items/").last
+      item_name = req.path.split("/items/").last
       
     elsif
       resp.write "Route not found"
