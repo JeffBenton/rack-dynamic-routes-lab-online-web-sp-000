56ff1b9138d6d10f4b04996e99675bbcf5332b87
Jeff Benton <sikletz@gmail.com>
2019-04-28 00:13:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Application
   
   @@items = []
@@ -8,6 +9,7 @@ class Application
     
     if req.path.match(/items/)
       item = req.path.split("/items").last
+      binding.pry
       if @@items.include?(item)
         resp.write "#{item.price}"
         resp.status = 200
