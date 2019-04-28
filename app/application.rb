20821f706c489c91f526a6bfd368a60f1d674579
Jeff Benton <sikletz@gmail.com>
2019-04-28 00:22:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find { |i| i.name = item_name }
+      binding.pry
       if item
         resp.write "#{item.price}"
         resp.status = 200
